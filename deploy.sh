143420ba4d0c1b716f0339437e639762cea0c9ea
Brodey Newman <brodeynewman@gmail.com>
2018-08-11 17:05:43 -0400

    adding ci/cd

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+if [ ${TRAVIS_BUILD_DIR} == "master" ]
+then
+  mkdir -p ~/.aws
+
+  cat > ~/.aws/credentials << EOL
+  [default]
+  aws_access_key_id = ${AWS_DEV_ACCESS_KEY}
+  aws_secret_access_key = ${AWS_DEV_SECRET_ACCESS_KEY}
+  EOL
+
+  aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://${okra-ui-dev}
+fi
