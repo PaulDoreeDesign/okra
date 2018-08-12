4734f1ae5ae6c42055f42752ab1017616545ccfd
Brodey Newman <brodeynewman@gmail.com>
2018-08-12 13:17:19 -0400

    modifying deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+if [ $TRAVIS_BRANCH == "master" ]
+then
 mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOF1
@@ -7,3 +9,17 @@ aws_secret_access_key = $AWS_DEV_SECRET_ACCESS_KEY
 EOF1
 
 aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$S3_DEV_BUCKET
+fi
+
+if [ $TRAVIS_BRANCH == "develop" ]
+then
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOF1
+[default]
+aws_access_key_id = foo
+aws_secret_access_key = foo
+EOF1
+
+aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$S3_DEV_BUCKET
+fi
