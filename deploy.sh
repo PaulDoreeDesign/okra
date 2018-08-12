1db179a2553c5d8cfc7d75cb6d6dbd43100bc114
Brodey Newman <brodeynewman@gmail.com>
2018-08-12 11:09:11 -0400

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOF1
 [default]
-aws_access_key_id = foo
-aws_secret_access_key = foo
+aws_access_key_id = $AWS_DEV_ACCESS_KEY
+aws_secret_access_key = $AWS_DEV_SECRET_ACCESS_KEY
 EOF1
 
-aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://foo
+aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://$S3_DEV_BUCKET
