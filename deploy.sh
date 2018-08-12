8819238f0508a5ea1283ab761a415f12a32c5b81
Brodey Newman <brodeynewman@gmail.com>
2018-08-11 22:05:40 -0400

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,14 @@
-if [ ${TRAVIS_BUILD_DIR} == "master" ]
+if [ "${TRAVIS_BUILD_DIR}" == "master" ];
 then
-  mkdir -p ~/.aws
+mkdir -p ~/.aws
 
-  cat > ~/.aws/credentials << EOL
-  [default]
-  aws_access_key_id = ${AWS_DEV_ACCESS_KEY}
-  aws_secret_access_key = ${AWS_DEV_SECRET_ACCESS_KEY}
-  EOL
+cat > ~/.aws/credentials << EOF1
+[default]
+aws_access_key_id = foo
+aws_secret_access_key = foo
+EOF1
 
-  aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://${okra-ui-dev}
+aws s3 sync ${TRAVIS_BUILD_DIR}/dist s3://foo
 elif [ ${TRAVIS_BUILD_DIR} == "master" ]
-  # Do something
-else
-  # Do something
+# Do something
 fi
